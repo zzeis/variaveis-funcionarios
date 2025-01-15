8f4ba230b31f7eb4df81ce924507750e8d91a2a1
zzeis <moisesmonteiro.stark@gmail.com>
2025-01-15 10:55:40 -0300

    feat: add funcion list

--- app/Http/Controllers/VariavelController.php
+++ app/Http/Controllers/VariavelController.php
@@ -13,6 +13,23 @@ class VariavelController extends Controller
         return view('variaveis.index', compact('variaveis'));
     }
 
+    public function list(Request $request)
+    {
+        $query = Variaveis::query();
+
+        // Filtro de pesquisa
+        if ($request->filled('search')) {
+            $search = $request->input('search');
+            $query->where('codigo', 'like', "%{$search}%")
+                ->orWhere('descricao', 'like', "%{$search}%");
+        }
+
+        // Paginação
+        $variaveis = $query->paginate(10);
+
+        return view('variaveis.list', compact('variaveis'));
+    }
+
     public function create()
     {
         return view('variaveis.create');
@@ -27,5 +44,29 @@ class VariavelController extends Controller
 
         Variaveis::create($request->all());
         return redirect()->route('variavel.index')->with('success', 'Variável cadastrada com sucesso!');
+    }
+
+    public function edit($id)
+    {
+        $variavel = Variaveis::findOrFail($id);
+        return view('variaveis.edit', compact('variavel'));
+    }
+    public function update(Request $request, $id)
+    {
+        $variavel = Variaveis::findOrFail($id);
+
+        $request->validate([
+            'codigo' => 'required|unique:variaveis,codigo,' . $variavel->id,
+            'descricao' => 'required|string|max:255',
+
+        ]);
+
+        $variavel->update([
+            'codigo' => $request->codigo,
+            'descricao' => $request->descricao,
+
+        ]);
+
+        return redirect()->route('variaveis.list')->with('success', 'Variável atualizada com sucesso!');
     }
 }
