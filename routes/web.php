c253cfd0e5165e3d3c2640e2d4ebf9c6a24f3e63
zzeis <moisesmonteiro.stark@gmail.com>
2025-01-15 10:34:24 -0300

    feat: add laravel breeze and views

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\FuncionarioController;
 use App\Http\Controllers\FuncionarioVariavelController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VariavelController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,17 +19,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-
+//funcionarios
 Route::get('/create/funcionario', [FuncionarioController::class, 'create'])->name('funcionario.create');
 Route::post('/store/funcionario', [FuncionarioController::class, 'store'])->name('funcionario.store');
+Route::get('/funcionarios', [FuncionarioController::class, 'index'])->name('funcionarios.index');
+Route::get('/funcionarios/{id}/edit', [FuncionarioController::class, 'edit'])->name('funcionarios.edit');
+Route::put('/funcionarios/{id}', [FuncionarioController::class, 'update'])->name('funcionarios.update');
 
+
+//variavel
+Route::get('/variaveis/list', [VariavelController::class, 'list'])->name('variaveis.list');
+Route::get('/variavel/{id}/edit', [VariavelController::class, 'edit'])->name('variavel.edit');
+Route::put('/variavel/{id}', [VariavelController::class, 'update'])->name('variavel.update');
 Route::get('/create/variavel', [VariavelController::class, 'create'])->name('variavel.create');
 Route::post('/store/variavel', [VariavelController::class, 'store'])->name('variavel.store');
 
-
-Route::get('/', [FuncionarioVariavelController::class, 'index'])->name('/');
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
 Route::post('/employee-variables', [FuncionarioVariavelController::class, 'store'])->name('employee-variables.store');
 
 Route::get('/employee-variables/export', [FuncionarioVariavelController::class, 'export'])->name('employee-variables.export');
@@ -38,3 +46,20 @@ Route::get('/api/search-variables', [ApiController::class, 'searchVariables']);
 Route::get('/api/employee-variables/{employeeId}', [ApiController::class, 'getEmployeeVariables']);
 Route::post('/employee-variables', [FuncionarioVariavelController::class, 'store'])->name('employee-variables.store');
 Route::delete('/employee-variables/{id}', [FuncionarioVariavelController::class, 'destroy'])->name('employee-variables.destroy');
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/variaveis', [FuncionarioVariavelController::class, 'index'])->name('variaveis');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__ . '/auth.php';
