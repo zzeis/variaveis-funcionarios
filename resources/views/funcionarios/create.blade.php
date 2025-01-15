7cb04dcb0a899bcf612267356bebba82c7aca133
zzeis <moisesmonteiro.stark@gmail.com>
2025-01-15 11:08:51 -0300

    feat: add view for funcionario create

--- resources/views/funcionarios/create.blade.php
+++ resources/views/funcionarios/create.blade.php
@@ -1,72 +1,90 @@
-<head>
-    <title>Novo funcionario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-</head>
-<style>
-    a {
-        text-decoration: none;
-    }
-</style>
+@extends('layouts.app')
 
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-<script>
-@if (session('success'))
-    Swal.fire({
-        icon: 'success',
-        title: 'Sucesso!',
-        text: "{{ session('success') }}",
-        timer: 3000, // Tempo em milissegundos (opcional)
-        showConfirmButton: false
-    });
-@endif
 
-@if (session('error'))
-    Swal.fire({
-        icon: 'error',
-        title: 'Erro!',
-        text: "{{ session('error') }}",
-        timer: 3000, // Tempo em milissegundos (opcional)
-        showConfirmButton: false
-    });
-@endif
-</script>
-<div class="container mt-5">
-    <a href="{{ route('/') }}"><i class="ri-home-2-line"></i></a>
-    <h1>Adicionar Funcionário</h1>
-    <form action="{{ route('funcionario.store') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label>Matrícula</label>
-            <input type="text" name="matricula" class="form-control" required>
+
+@section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        <div class="form-group">
-            <label>Nome</label>
-            <input type="text" name="nome" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>Cargo</label>
-            <input type="text" name="cargo" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>Diretoria</label>
-            <input type="text" name="diretoria" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>Seção</label>
-            <input type="text" name="secao" class="form-control" required>
-        </div>
-        <button type="submit" class="btn btn-success mt-4">Salvar</button>
-    </form>
-</div>
+    @endif
+    <script>
+        @if (session('success'))
+            Swal.fire({
+                icon: 'success',
+                title: 'Sucesso!',
+                text: "{{ session('success') }}",
+                timer: 3000, // Tempo em milissegundos (opcional)
+                showConfirmButton: false
+            });
+        @endif
+
+        @if (session('error'))
+            Swal.fire({
+                icon: 'error',
+                title: 'Erro!',
+                text: "{{ session('error') }}",
+                timer: 3000, // Tempo em milissegundos (opcional)
+                showConfirmButton: false
+            });
+        @endif
+    </script>
+
+    <div class="container mx-auto mt-5 p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+        <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200 mb-4">
+            Adicionar Funcionário
+        </h2>
+        <form id="func" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4"
+            action="{{ route('funcionario.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">Matrícula
+                </label>
+                <input type="text" name="matricula"
+                    class="block w-full rounded-md border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+            </div>
+            <div>
+                <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                    Nome
+                </label>
+                <input type="text" name="nome"
+                    class="block w-full rounded-md border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+            </div>
+            <div>
+                <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                    Cargo
+                </label>
+                <input type="text" name="cargo"
+                    class="block w-full rounded-md border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+            </div>
+            <div class="form-group">
+                <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                    Diretoria
+                </label>
+                <input type="text" name="diretoria"
+                    class="block w-full rounded-md border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+            </div>
+            <div class="form-group">
+                <label class="block text-sm font-medium text-gray-700 dark:text-gray-300">Seção
+                </label>
+                <input type="text" name="secao"
+                    class="block w-full rounded-md border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+            </div>
+
+
+        </form>
+        <button type="submit" form="func"
+            class=" mt-2 bg-indigo-600 text-white dark:bg-indigo-500 hover:bg-indigo-700 dark:hover:bg-indigo-600 px-4 py-2 rounded-md flex items-center">
+
+            Salvar</button>
+    </div>
+@endsection
