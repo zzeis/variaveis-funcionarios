7d5fde02becfcc1094f626ae86a9c9074fd8906d
zzeis <moisesmonteiro.stark@gmail.com>
2025-01-15 10:37:36 -0300

    feat: add search at index funcion

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -7,9 +7,20 @@ use Illuminate\Http\Request;
 
 class FuncionarioController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $funcionarios = Funcionario::all();
+        $query = Funcionario::query();
+
+        // Filtro de pesquisa
+        if ($request->filled('search')) {
+            $search = $request->input('search');
+            $query->where('nome', 'like', "%{$search}%")
+                ->orWhere('matricula', 'like', "%{$search}%");
+        }
+
+        // Paginação
+        $funcionarios = $query->paginate(10);
+
         return view('funcionarios.index', compact('funcionarios'));
     }
 
@@ -30,5 +41,33 @@ class FuncionarioController extends Controller
 
         Funcionario::create($request->all());
         return redirect()->route('funcionarios.index')->with('success', 'Funcionário cadastrado com sucesso!');
+    }
+
+    public function edit($id)
+    {
+        $funcionario = Funcionario::findOrFail($id);
+        return view('funcionarios.edit', compact('funcionario'));
+    }
+    public function update(Request $request, $id)
+    {
+        $funcionario = Funcionario::findOrFail($id);
+
+        $request->validate([
+            'matricula' => 'required|unique:funcionarios,matricula,' . $funcionario->id,
+            'nome' => 'required|string|max:255',
+            'cargo' => 'required|string|max:255',
+            'diretoria' => 'required|string|max:255',
+            'secao' => 'required|string|max:255',
+        ]);
+
+        $funcionario->update([
+            'nome' => $request->nome,
+            'matricula' => $request->matricula,
+            'cargo' => $request->cargo,
+            'diretoria' => $request->diretoria,
+            'secao' => $request->secao,
+        ]);
+
+        return redirect()->route('funcionarios.index')->with('success', 'Funcionário atualizado com sucesso!');
     }
 }
